2626d9589c27c6f4bbbba6146a6391e05cf2328a
KKamjangE <ajm980718@gmail.com>
2023-11-01 18:22:34 +0900

    Feat: 로그인 컨트롤러 구현

--- src/main/java/com/toy/web/controller/UserController.java
+++ src/main/java/com/toy/web/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.toy.web.controller;
 
 import com.toy.web.dto.JoinRequest;
+import com.toy.web.dto.LoginRequest;
 import com.toy.web.response.ResponseData;
 import com.toy.web.response.ResponseMessage;
 import com.toy.web.response.StatusCode;
+import com.toy.web.service.JsonWebTokenService;
 import com.toy.web.service.UserService;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,26 +15,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/sign")
 public class UserController {
 
     private final UserService userService;
 
+    private final JsonWebTokenService jsonWebTokenService;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JsonWebTokenService jsonWebTokenService) {
         this.userService = userService;
+        this.jsonWebTokenService = jsonWebTokenService;
     }
 
     @PostMapping("/in")
-    public void signIn() { // 로그인
+    public ResponseEntity<ResponseData>  signIn(LoginRequest loginRequest) { // 로그인
+
+        String userName = userService.loginCheck(loginRequest);
+
+        if(userName == null) {
+            return new ResponseEntity<>(ResponseData.res(StatusCode.NOT_FOUND, ResponseMessage.SIGN_IN_FAIL), HttpStatus.OK);
+        } else  {
+            String token = jsonWebTokenService.generateToken(loginRequest.getUserId());
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("jwt", token);
+            responseBody.put("userName", userName);
+            return new ResponseEntity<>(ResponseData.res(StatusCode.OK, ResponseMessage.SIGN_IN_SUCCESS, responseBody), HttpStatus.OK);
+        }
+
     }
 
     @PostMapping("/up")
     public ResponseEntity<ResponseData> signUp(JoinRequest joinRequest) { // 회원가입
 
         JoinRequest checkJoinRequest = userService.findByUserId(joinRequest.getUserId());
-
         String encodedPassword = BCrypt.hashpw(joinRequest.getPassword(), BCrypt.gensalt());
         joinRequest.setPassword(encodedPassword);
 
