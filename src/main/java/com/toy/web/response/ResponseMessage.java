3c6d3ede86a96f908923a16a0e9f2f69d511a42f
KKamjangE <ajm980718@gmail.com>
2023-11-06 17:17:42 +0900

    Feat: center api 관련 메시지 추가

--- src/main/java/com/toy/web/response/ResponseMessage.java
+++ src/main/java/com/toy/web/response/ResponseMessage.java
@@ -6,4 +6,7 @@ public class ResponseMessage {
     public static final String SIGN_UP_SUCCESS = "회원가입 성공.";
     public static final String SIGN_UP_FAIL = "회원가입 실패.";
     public static final String ALREADY_USER = "이미 존재하는 아이디입니다.";
+    public static final String GET_CENTERS_SUCCESS = "데이터를 가져왔습니다.";
+    public static final String POST_CENTER_SUCCESS = "데이터를 저장했습니다.";
+    public static final String DELETE_CENTER_SUCCESS = "데이터를 삭제했습니다.";
 }
