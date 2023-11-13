82c3f27b2b57e274cbf7af36d599e145626291ff
KKamjangE <ajm980718@gmail.com>
2023-11-13 15:07:10 +0900

    Feat: center repository에 커스텀 메서드 생성

--- src/main/java/com/toy/web/repository/CenterRepository.java
+++ src/main/java/com/toy/web/repository/CenterRepository.java
@@ -4,6 +4,12 @@ import com.toy.web.dto.CenterRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CenterRepository extends MongoRepository<CenterRequest, String> {
+    List<CenterRequest> findAllByUserName(String userName);
+    CenterRequest findByUserNameAndId(String userName, String id);
+
+    CenterRequest deleteByUserNameAndId(String userName, String id);
 }
