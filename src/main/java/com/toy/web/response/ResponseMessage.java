063fc198e7559a19caaa953eace4db4bd0935447
KKamjangE <ajm980718@gmail.com>
2023-11-13 15:07:46 +0900

    Feat: 응답 메시지 추가

--- src/main/java/com/toy/web/response/ResponseMessage.java
+++ src/main/java/com/toy/web/response/ResponseMessage.java
@@ -8,5 +8,7 @@ public class ResponseMessage {
     public static final String ALREADY_USER = "이미 존재하는 아이디입니다.";
     public static final String GET_CENTERS_SUCCESS = "데이터를 가져왔습니다.";
     public static final String POST_CENTER_SUCCESS = "데이터를 저장했습니다.";
+    public static final String POST_CENTER_CONFLICT = "이미 저장된 데이터입니다.";
     public static final String DELETE_CENTER_SUCCESS = "데이터를 삭제했습니다.";
+    public static final String DELETE_CENTER_FAIL = "데이터 삭제에 실패했습니다.";
 }
