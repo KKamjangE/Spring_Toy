b7a57eb66bac3296be9aac11dc5c8445f83f4fd7
KKamjangE <ajm980718@gmail.com>
2023-12-08 17:41:15 +0900

    Feat: CORS 허용 주소 추가

--- src/main/java/com/toy/web/config/WebConfig.java
+++ src/main/java/com/toy/web/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer { // web mvc 설정 파일
     @Override // 메서드 오버라이딩
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // endpoint
-                .allowedOrigins("http://localhost:5173") // 허용할 origin
+                .allowedOrigins("http://localhost:5173", "https://www.react-toy.store") // 허용할 origin
                 .allowedMethods("*") // 허용할 메서드
                 .allowCredentials(true) // 쿠키 인증 요청 여부
                 .maxAge(3000); // pre-flight 리퀘스트 캐싱 시간
